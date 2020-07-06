78ff9d4aeacb2cf4861913ae8accd5a533a0cbf5
kashishnagpal <kashishnagpal10@gmail.com>
2020-07-07 01:23:43 +0530

    Commit

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Kashish Nagpal",
+    "role": "Frontend Developer ",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/kashishnagpal-67ba08121/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/kashishnagpal",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/kashishnagpal",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a final year student at Bangalore Institute of Technology and pursuing my B.Tech from here. I am a self taught Web Developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"Bangalore Institute of Technology",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
